6e6b0e2fb2ddf2754440870394efd5c2e9fdde81
stoneBeast <1519696648@qq.com>
2024-04-17 10:40:33 +0800

    first commit

---
+++ main/components/ble_mesh/ble_mesh.c
@@ -0,0 +1,48 @@
+#include "ble_mesh.h"
+#include "mesh_config.h"
+
+esp_err_t ble_mesh_init(void)
+{
+    uint8_t match[2] = {0xdd, 0xdd};
+    esp_err_t err = ESP_OK;
+
+    prov_key.net_idx = ESP_BLE_MESH_KEY_PRIMARY;
+    prov_key.app_idx = APP_KEY_IDX;
+    memset(prov_key.app_key, APP_KEY_OCTET, sizeof(prov_key.app_key));
+
+    esp_ble_mesh_register_prov_callback(example_ble_mesh_provisioning_cb);
+    esp_ble_mesh_register_config_client_callback(example_ble_mesh_config_client_cb);
+    esp_ble_mesh_register_generic_client_callback(example_ble_mesh_generic_client_cb);
+
+    err = esp_ble_mesh_init(&provision, &composition);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to initialize mesh stack (err %d)", err);
+        return err;
+    }
+
+    err = esp_ble_mesh_provisioner_set_dev_uuid_match(match, sizeof(match), 0x0, false);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to set matching device uuid (err %d)", err);
+        return err;
+    }
+
+    err = esp_ble_mesh_provisioner_prov_enable(ESP_BLE_MESH_PROV_ADV | ESP_BLE_MESH_PROV_GATT);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to enable mesh provisioner (err %d)", err);
+        return err;
+    }
+
+    err = esp_ble_mesh_provisioner_add_local_app_key(prov_key.app_key, prov_key.net_idx, prov_key.app_idx);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to add local AppKey (err %d)", err);
+        return err;
+    }
+
+    ESP_LOGI(TAG, "BLE Mesh Provisioner initialized");
+
+    return err;
+}
---
+++ main/components/ble_mesh/ble_mesh.h
@@ -0,0 +1,8 @@
+#ifndef _BLE_MESH_H_
+#define _BLE_MESH_H_
+
+#include "esp_err.h"
+
+esp_err_t ble_mesh_init(void);
+
+#endif
