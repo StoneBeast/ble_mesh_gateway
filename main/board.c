4e2d7ae87ced92ac5cbb5ae69d93351c14a521ce
stoneBeast <1519696648@qq.com>
2024-04-21 00:26:13 +0800

    完成led以及button的初始函数

--- main/board.c
+++ main/board.c
@@ -1,11 +1,12 @@
 #include "board.h"
+#include "driver/gpio.h"
 
 /*
     指示灯:
-        正常工作    常亮
-        有操作发生  快速、单次闪烁
-        接收广播    快速闪烁
-        可以考虑使用pwm输出
+        正常工作      缓慢闪烁
+        正在添加节点   常亮
+        接收广播      快速闪烁
+        使用信号量或队列，当触发状态改变时唤醒任务
 
     按钮:
         开启/关闭接收广播:  长按(使用定时器)
@@ -13,12 +14,37 @@
 
 static void board_led_init(void)
 {
-    //  如果可以，使用pwm输出
+    gpio_reset_pin(LED_PIN);
+
+    gpio_config_t *led_config_t = (gpio_config_t *)malloc(sizeof(gpio_config_t));
+
+    led_config_t->intr_type = GPIO_INTR_DISABLE;
+    led_config_t->mode = GPIO_MODE_OUTPUT;
+    led_config_t->pin_bit_mask = PIN_MASK(LED_PIN);
+    led_config_t->pull_down_en = GPIO_PULLDOWN_DISABLE;
+    led_config_t->pull_up_en = GPIO_PULLUP_DISABLE;
+
+    gpio_config(led_config_t);
+
+    free(led_config_t);
 }
 
 static void board_button_init(void)
 {
     //  引入定时器，检测按下时间
+    gpio_reset_pin(USER_BUTTON_PIN);
+
+    gpio_config_t *user_button_config_t = (gpio_config_t *)malloc(sizeof(gpio_config_t));
+
+    user_button_config_t->intr_type = GPIO_INTR_ANYEDGE;
+    user_button_config_t->mode = GPIO_MODE_INPUT;
+    user_button_config_t->pin_bit_mask = PIN_MASK(USER_BUTTON_PIN);
+    user_button_config_t->pull_down_en = GPIO_PULLDOWN_ENABLE;
+    user_button_config_t->pull_up_en = GPIO_PULLUP_DISABLE;
+
+    gpio_config(user_button_config_t);
+
+    free(user_button_config_t);
 }
 
 void board_init(void)
--- main/board.h
+++ main/board.h
@@ -6,6 +6,8 @@
 #define LED_PIN         GPIO_NUM_13
 #define USER_BUTTON_PIN GPIO_NUM_12
 
+#define PIN_MASK(pin) ((uint64_t)(1 << pin))
+
 void board_init(void);
 
 #endif // _BOARD_H_
